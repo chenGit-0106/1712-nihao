58713ec8b9dad9f2a50d54ff53236914350a9137
chenteng <806975529@qq.com>
2020-03-25 09:06:54 +0800

    esplise忽略提交

---
+++ src/main/java/com/bw/controller/nihaocontroller.java
@@ -0,0 +1,14 @@
+package com.bw.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class nihaocontroller {
+
+	@RequestMapping("hello")
+	public String hello() {
+		
+		return "hello";
+	}
+}
