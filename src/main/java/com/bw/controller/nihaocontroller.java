edb1c5bab99c2a2e991f0533b52702d9da4b9b0a
jiadonghua <1758763945@qq.com>
2020-03-25 10:04:22 +0800

    我改了哈哈哈哈!!!!

--- src/main/java/com/bw/controller/nihaocontroller.java
+++ src/main/java/com/bw/controller/nihaocontroller.java
@@ -5,7 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class nihaocontroller {
-
+	/* 我很快乐,啦啦啦啦 */
+	/* 哈哈哈哈哈哈哈哈 */
 	@RequestMapping("hello")
 	public String hello() {
 		
